f23e63a3967d0e2fca4b458abb93c6e623dce3fc
millerd <millerdicarlo@gmail.com>
2022-08-02 22:01:38 -0400

    inquirer working

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+//const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+//const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
 
 
+/* 
 fs.writeFile('./index.html', generatePage(name, github), err => {
   if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
